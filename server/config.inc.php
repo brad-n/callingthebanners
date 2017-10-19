8cd8712371576297b11dfdba0417caac52171ccf
Brad Norman <bnorman@aplustree.com>
2017-10-19 12:14:52 -0700

    add base config file

---
+++ server/config.inc.php
@@ -0,0 +1,11 @@
+<?php 
+$cfg['db_user'] = 'db_user';
+$cfg['db_pass'] = 'db_pass';
+
+$cfg['mailgun_api'] = 'mailgun-api-key';
+$cfg['mailgun_domain'] = 'mailgun-domain';
+
+
+$GLOBALS['cfg'] = $cfg;
+
+?>
